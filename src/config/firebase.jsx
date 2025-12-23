c5c2b04ce38b27be3284237e3c6476f0add5e00d
KIRAMA97 <officialthiam97@gmail.com>
2025-12-23 15:40:11 +0000

    commit register form

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -3,12 +3,19 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY ,
-  authDomain:  import.meta.env.VITE_AUTH_DOMAIN ,
-  projectId:  import.meta.env.VITE_PROJECT_ID,
-  storageBucket:  import.meta.env.VITE_STORAGE_BUCKET ,
-  messagingSenderId:  import.meta.env.VITE_SENDER_ID,
-  appId:  import.meta.env.VITE_APP_ID 
+  // apiKey: import.meta.env.VITE_API_KEY ,
+  // authDomain:  import.meta.env.VITE_AUTH_DOMAIN ,
+  // projectId:  import.meta.env.VITE_PROJECT_ID,
+  // storageBucket:  import.meta.env.VITE_STORAGE_BUCKET ,
+  // messagingSenderId:  import.meta.env.VITE_SENDER_ID,
+  // appId:  import.meta.env.VITE_APP_ID 
+  
+   apiKey: "AIzaSyBSS4q6kYTJI_pXk3DSwZg-cynl1n4sRJo",
+   authDomain: "carsend-852f3.firebaseapp.com",
+   projectId: "carsend-852f3",
+   storageBucket: "carsend-852f3.firebasestorage.app",
+   messagingSenderId: "756318161488",
+   appId: "1:756318161488:web:c0d2e73404794056e65f55" 
   
 };
 
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -2,40 +2,149 @@ import React, { useState } from 'react'
 import {auth} from "../../config/firebase"
 import {createUserWithEmailAndPassword} from "firebase/auth"
 
+import { CiUser } from "react-icons/ci";
+import { MdOutlineAttachEmail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
 
 
-const Register = () => {
+const Register = (e) => {
 
   const [email ,setEmail] = useState("")
   const [password ,setPassword] = useState("")
 
-   const inscrir = async () => {
+   const inscrire = async (e) => {
       
-     if(!email || !password ) return ;
-    await createUserWithEmailAndPassword(auth , email , password)
+   e.preventDefault()
+     
+    //  if(!email == "" || !password == "") {
+    //   alert("veuillez remplir tous les champs")
+
+    //   return
+    //  }
+     try {
+       const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+       )
+       console.log("utlisateur cree :" , userCredential.user)
+     } catch(error) {
+       console.log(error.code , error.message)
+     }
+   
 
    }
 
 
   return (
   
-       <div className="max-w-xl mx-auto p-4 rounded-lg bg-black/40 text-black ">
-       <h1 className='text-center text2xl font-bold mb-5'>Creer un compte</h1>
-        <form action="" className='flex flex-col gap-4'>
-          <input type="email" className='px-3 py-1 text-md ' placeholder="email..."
-           value={email}
-           onChange={(e) => setEmail(e.target.value)}
-          
-          />
-          <input type="password" className='px-3 py-1 text-md ' placeholder="mots de passe..."
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
-          
-          />
-          <button className='bg-black text-white' onClick={inscrir}  type="submit" >s'inscrire</button>
+       <div className="w-screen h-[100vh] p-[2%] bg-gradient-to-r from-sky-50 to-blue-200">
+          <div className="flex overflow-hidden w-full h-full bg-gradient-to-l from-neutral-50 to-blue-100 rounded-md shadow-sm border-2 border-neutral-100 ">
+              <div className=" flex flex-col justify-center gap-10 px-10 py-5 w-1/2 max-sm-hidden ">
+                 <h1 className="text-6xl text-blue-500 font-bold ">CarSend</h1>
+                 <h3 className='text-4xl max-w-xl text-dark font-semibold '>Ton monde automobile, réuni sur une seule plateforme.</h3>
+                 <p className="text-md text-neutral-500">Simple, rapide et fiable, elle vous permet de réaliser tous vos projets automobiles en toute confiance, avec une expérience moderne adaptée à vos besoins.</p>
+              </div>
+              <div className="w-1/2 p-5 ">
 
-        </form>
-    </div>
+                 <div className=" h-full rounded-lg shadow-xl bg-white py-10 px-10">
+
+                    <h3 className='font-bold text-xl mb-2'>Bienvenue</h3>
+                    <p className='text-neutral-500'>Inscris toi pour acceder a la plaform</p>
+
+                    <form  className='mt-10'>
+
+                     {/* nom et prenom */}
+
+                        <div className="grid grid-cols-2 gap-3">
+
+                           <div className="input flex flex-col ">
+                              <label className="text-sm"  htmlFor="prenom">Prenom</label>
+                               <div className="shadow-sm mt-2 group rounded-xl px-3 overflow-hidden  flex items-center  border-2 border-neutral-200 focus-within:border-blue-300 focus-within:ring focus-within:ring-blue-200">
+                                    <CiUser className="text-xl text-neutral-800  group-focus-within:text-blue-500" />                      
+
+                                    <input 
+                                    className="py-2 text-sm px-3 w-full focus:outline-none"
+                                    type="text" 
+                                    placeholder="prenom"
+                                    />
+                                 </div>                     
+                           </div>
+                           <div className="input flex flex-col ">
+                              <label className="text-sm" htmlFor="nom">Nom</label>
+                               <div className="shadow-sm mt-2 group rounded-xl px-3 overflow-hidden  flex items-center   border-2 border-neutral-200 focus-within:border-blue-300 focus-within:ring focus-within:ring-blue-200">
+                                    <CiUser className="text-xl text-neutral-800  group-focus-within:text-blue-500" />                      
+
+                                    <input 
+                                    className="py-2 text-sm px-3 w-full focus:outline-none"
+                                    type="text" 
+                                    placeholder="nom"
+                                    />
+                                 </div>                     
+                           </div>
+
+                        </div>
+                     {/* email */}
+
+                      
+                        <div className="input flex flex-col mt-5 ">
+                           <label className="text-sm " htmlFor="email">Email</label>
+                              <div className="shadow-sm mt-2 group rounded-xl px-3 overflow-hidden  flex items-center   border-2 border-neutral-200 focus-within:border-blue-300 focus-within:ring focus-within:ring-blue-200">
+                                 <MdOutlineAttachEmail  className="text-xl text-neutral-800  group-focus-within:text-blue-500" />                      
+                             
+
+                                 <input 
+                                    className="py-2 text-sm px-3 w-full focus:outline-none"
+                                    type="emal" 
+                                    placeholder="toi@exemple.com"
+                                    />
+                              </div>                     
+                        </div>
+
+                        
+                     {/* mots de passe et confirm mots de passe*/}
+
+                        <div className="grid grid-cols-2 gap-3 mt-5">
+
+                           <div className="input flex flex-col ">
+                              <label className="text-sm"  htmlFor="password">Mots de Passe</label>
+                               <div className="shadow-sm mt-2 group rounded-xl px-3 overflow-hidden  flex items-center  border-2 border-neutral-200 focus-within:border-blue-300 focus-within:ring focus-within:ring-blue-200">
+                                    <MdPassword  className="text-xl text-neutral-800  group-focus-within:text-blue-500" />                      
+
+                                    <input 
+                                    className="py-2 text-sm px-3 w-full focus:outline-none"
+                                    type="password" 
+                                    placeholder="xxxxxxx"
+                                    />
+                                 </div>                     
+                           </div>
+                           <div className="input flex flex-col ">
+                              <label className="text-sm" htmlFor="confirmpassword">Confirmé mots de passe</label>
+                               <div className="shadow-sm mt-2 group rounded-xl px-3 overflow-hidden  flex items-center   border-2 border-neutral-200 focus-within:border-blue-300 focus-within:ring focus-within:ring-blue-200">
+                                    <MdPassword  className="text-xl text-neutral-800  group-focus-within:text-blue-500" />                      
+
+                                    <input 
+                                    className="py-2 text-sm px-3 w-full focus:outline-none"
+                                    type="password" 
+                                    placeholder="xxxxxx"
+                                    />
+                                 </div>                     
+                           </div>
+
+                        </div>
+                         
+                         <button className="w-full text-sm text-white py-2 mt-10 text-center bg-blue-500 rounded-xl shadow-sm hover:bg-blue-600">
+                            Inscription
+                         </button>
+            
+ 
+                    </form>
+                   
+                 </div>
+                
+              </div>
+          </div>
+       </div>
    
   )
 }
